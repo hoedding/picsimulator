5cdf908f158ccebba0d3042eb699fb654c8767cc
MBielmeier <mexle.b@web.de>
2014-05-14 13:31:28 +0200

    breakpoints visuell

--- src/picsim/mvc/view/BreakpointCellRenderer.java
+++ src/picsim/mvc/view/BreakpointCellRenderer.java
@@ -1,26 +1,56 @@
 package picsim.mvc.view;
 
+import java.awt.Color;
 import java.awt.Component;
-import java.awt.Label;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-public class BreakpointCellRenderer extends Label implements ListCellRenderer<String>{
+public class BreakpointCellRenderer extends JLabel implements ListCellRenderer<String>{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7082125848079406969L;
+	
+	public List<Integer> bplist = new ArrayList<>();
 
-	public BreakpointCellRenderer() {
+    private JList<String> ss;
+
+	public BreakpointCellRenderer(JList<String> s) {
+		
+		setOpaque(true);
+
+        ss = s;
 		
 		
 	}
 
 	@Override
-	public Component getListCellRendererComponent(JList<? extends String> arg0,
-			String arg1, int arg2, boolean arg3, boolean arg4) {
-		// TODO Auto-generated method stub
-		return null;
+	public Component getListCellRendererComponent(JList<? extends String> list, String value, int index, boolean isSelected, boolean cellHasFocus) {
+		setText(value.toString());
+
+		 
+
+        if (bplist.contains(index)) setBackground(Color.RED);
+
+        else if (isSelected) setBackground(Color.CYAN);
+
+        else setBackground(Color.WHITE);
+
+ 
+
+        return this;
 	}
+	public void setThings(List<Integer> s) {
+
+        bplist = s;
+
+        
+
+        ss.repaint();
+
+    }
 }
